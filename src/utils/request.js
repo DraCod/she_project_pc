658e974616e908bed4233544b487c300f7f859d6
DroCodZhang <1564040820@qq.com>
2020-12-06 13:39:29 +0800

    首次提交

---
+++ src/utils/request.js
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { Message } from 'element-ui'
+import store from '@/store'
+import router from 'vue-router'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
+
+service.interceptors.request.use(
+  config => {
+    if (store.getters.token) {
+      config.headers['authorization'] = getToken()
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+    if (res.status === 401) {
+      router.replace('/login')
+      Message({
+        message: '请先登录',
+        type: 'info',
+        duration: 5 * 1000
+      })
+    } else if (res.status === 402) {
+      Message({
+        message: res.msg,
+        type: 'error',
+        duration: 5 * 1000
+      })
+      return res
+    } else {
+      return res
+    }
+  },
+  error => {
+    console.log('err' + error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+export default service
