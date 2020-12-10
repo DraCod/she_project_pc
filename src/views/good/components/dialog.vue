551a3322878a63aea8928a38eb95315c53c9910f
DroCodZhang <1564040820@qq.com>
2020-12-10 09:52:35 +0800

    每日提交

--- src/views/good/components/dialog.vue
+++ src/views/good/components/dialog.vue
@@ -1,14 +1,23 @@
 <template>
   <el-dialog title="添加" :visible.sync="dialog" width="30%">
-    <el-upload
-      class="avatar-uploader"
-      :action=" url+ 'pc/upload'"
-      :show-file-list="false"
-      :on-success="handleAvatarSuccess"
-    >
-      <img v-if="form.img_url" :src="form.img_url" class="avatar" />
-      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-    </el-upload>
+    <el-form :model="form" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="商品名称" prop="goods">
+        <el-input v-model="form.goods" class="is-width"></el-input>
+      </el-form-item>
+      <el-form-item label="分类" prop="class_id">
+        <el-select v-model="form.class_id" placeholder="请选择" class="is-width">
+          <el-option
+            v-for="item in class_list"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="主图" prop="main_id">
+        <upload-img :src="form.main_url" @upload="upload('main',$event)"></upload-img>
+      </el-form-item>
+    </el-form>
     <span slot="footer" class="dialog-footer">
       <el-button @click="dialog = false">取 消</el-button>
       <el-button type="primary" @click="dialog = false">确 定</el-button>
@@ -17,27 +26,35 @@
 </template>
 
 <script>
-const url=process.env.VUE_APP_BASE_API
+import {classifyList} from '@/api/classify'
 export default {
   props: {
     value: {},
   },
   data() {
     return {
-      dialog: false,
-      url,
+      dialog: true,
       form:{
-          img_id:"",
-          img_url:''
-      }
+        goods:"",
+        class_id:'',//分类id
+        main_url:'',//主图路径
+        main_id:'',//主图id
+      },
+      class_list:[
+
+      ]
     };
   },
+  mounted(){
+    classifyList().then(res=>{
+      this.class_list=res.data.map(ro=>({label:ro.classify,value:ro.id}))
+    })
+  },
   methods: {
-    handleAvatarSuccess(res, file) {
-    //   console.log(res, file);
-      this.form.img_url=this.url+res.path;
-      this.form.img_id=res.id;
-    },
+    upload(key,{url,id}){
+      this.form[key+'_url']=url
+      this.form[key+'_id']=id
+    }
   },
   watch: {
     value(val) {
@@ -47,31 +64,14 @@ export default {
       this.$emit("input", val);
     },
   },
+  components:{
+    uploadImg:require('./upload-img.vue').default
+  }
 };
 </script>
 
 <style lang='scss' scoped>
-  .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
+  .is-width{
+    width:250px;
   }
 </style>
---
+++ src/views/good/components/upload-img.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-upload
+    class="avatar-uploader"
+    :action="url + 'pc/upload'"
+    :show-file-list="false"
+    :on-success="handleAvatarSuccess"
+  >
+    <img v-if="src" :src="src" class="avatar" />
+    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+  </el-upload>
+</template>
+
+<script>
+const url = process.env.VUE_APP_BASE_API;
+export default {
+  props: ["src"],
+  data() {
+    return {
+      url,
+    };
+  },
+  methods: {
+    handleAvatarSuccess(res, file) {
+        this.$emit('upload',{
+            url:this.url + res.path,
+            id:res.id
+        })
+    //   this.form.img_url = this.url + res.path;
+    //   this.form.img_id = res.id;
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
